2743f77de0a59b7663737fc6f49d2176190a7faf
kerimcalik <kcalik@gmail.com>
2021-10-27 21:52:50 +0300

    updated

--- ObjectOriatentedPrograming/Program.cs
+++ ObjectOriatentedPrograming/Program.cs
@@ -2,16 +2,73 @@
 
 namespace ObjectOriatentedPrograming
 {
-    public class Program1
+    public class Program
     {
+        static void ForOlustur()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calisan calisan = new Calisan(321313, "kerim","çalık");
+           
         }
 
-        private static void BaskaProjeProseduru()
+
+    }
+
+    public class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public long TcNo { get; set; }
+        
+        public static void IseAl()
         {
 
         }
+
+        public Calisan(int name, string surname)
+        {
+
+        }
+
+       
+        public Calisan(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+        }
+
+        public Calisan(string ad, string soyad, long tc)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            TcNo = tc;
+        }
+    }
+
+    public class OzelCalisan: Calisan
+    {
+        public int OzelCalisanId { get; set; }
+    }
+
+    public class TamZamanliCalisan: OzelCalisan
+    {
+        
+    }
+
+    public class YariZamanliCalisan : Calisan
+    {
+
+    }
+
+    public class StajyerliCalisan : Calisan
+    {
+
     }
 }
